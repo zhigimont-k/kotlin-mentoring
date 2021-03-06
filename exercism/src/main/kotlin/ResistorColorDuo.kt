9ebc207e6623a4804e96802704bb8925b5589da4
zhigimont-k <zhigimont.k@gmail.com>
2021-03-06 21:36:20 +0300

    Start exercism tasks

---
+++ exercism/src/main/kotlin/ResistorColorDuo.kt
@@ -0,0 +1,7 @@
+object ResistorColorDuo {
+    fun value(vararg colors: Color): Int {
+        return (ResistorColor.colorCode(colors[0].toString()).toString()
+                +ResistorColor.colorCode(colors[1].toString()).toString())
+            .toInt()
+    }
+}
---
+++ exercism/src/main/kotlin/TwoFer.kt
@@ -0,0 +1,7 @@
+internal fun twofer(name: String): String {
+    return "One for ${name}, one for me."
+}
+
+internal fun twofer(): String {
+    return "One for you, one for me."
+}
